e0a443d9c118372040517b755fb2bb5b74dd0af8
Quentin Rasmont <qrasmont@gmail.com>
2021-11-11 09:55:06 +0100

    Check if .projectile.json is present in cwd

--- projectile.go
+++ projectile.go
@@ -6,15 +6,30 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
+
+func hasConfigFile(workdir *string) bool {
+
+	matches, err := filepath.Glob(*workdir + "/.projectile.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if len(matches) == 1 {
+		return true
+	}
+	return false
+}
 
 func main() {
 	path := flag.String("p", "", "The project's path.")
 
 	flag.Parse()
-	action := flag.Args()
+	actions := flag.Args()
 
-	fmt.Printf("path: %s, action: %s\n", *path, action)
+	fmt.Printf("path: %s, action: %s\n", *path, actions)
 
 	workdir := ""
 	if *path != "" {
@@ -26,6 +41,10 @@ func main() {
 			log.Fatal(errors.New("Could not get cwd."))
 		}
 		workdir = cwd
+	}
+
+	if !hasConfigFile(&workdir) {
+		log.Fatal(errors.New("No .projectile.json found!"))
 	}
 
 	fmt.Printf("workdir set to: %s\n", workdir)
