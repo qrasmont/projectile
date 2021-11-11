0d95322376cd43219553bb180f6999de884d16de
Quentin Rasmont <qrasmont@gmail.com>
2021-11-11 09:32:16 +0100

    Get work dir from flag or cwd

--- projectile.go
+++ projectile.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 	path := flag.String("p", "", "The project's path.")
-	flag.Parse()
 
+	flag.Parse()
 	action := flag.Args()
 
 	fmt.Printf("path: %s, action: %s\n", *path, action)
+
+	workdir := ""
+	if *path != "" {
+		workdir = *path
+	} else {
+		// Get the cwd
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Fatal(errors.New("Could not get cwd."))
+		}
+		workdir = cwd
+	}
+
+	fmt.Printf("workdir set to: %s\n", workdir)
 }
