6158772e8ed4e6d945d266521b81dc67afabde20
Quentin Rasmont <qrasmont@gmail.com>
2022-03-03 21:57:28 +0100

    Add the command root

--- projectile.go
+++ projectile.go
@@ -1,35 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/quadstew/projectile/cmd"
-	"github.com/quadstew/projectile/project"
 )
 
-func Bye(err error) {
-
-	fmt.Println(err)
-	os.Exit(1)
-}
-
 func main() {
-	// Parse cmd args & flags
-	cmdConfig, err := cmd.New()
-	if err != nil {
-		Bye(err)
-	}
-
-    // Parse the project config & set the actions to perform
-	err = project.Init(&cmdConfig)
-	if err != nil {
-		Bye(err)
-	}
-
-    // Run actions
-	err = project.Run()
-	if err != nil {
-		Bye(err)
-	}
+	cmd.Execute()
 }
